8aa5eb314ce01cf8e1600b619556b138f9624f76
beckoduje <56414164+beckoduje@users.noreply.github.com>
2021-02-20 19:58:02 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 const menuBtn = document.querySelector(".menu-btn");
 const overlay = document.querySelector(".nav-items-overlay--list");
 const header = document.querySelector(".header-primary");
+const scrollDown = document.querySelector(".scroll-down-icon");
 let menuOpen = false;
 menuBtn.addEventListener("click", () => {
-    if(!menuOpen) {
-        menuBtn.classList.add("open");
-        overlay.classList.add("open-list");
-        header.classList.add("close");
-        menuOpen = true;
-    } else {
-        menuBtn.classList.remove("open");
-        overlay.classList.remove("open-list");
-        header.classList.remove("close");
-        menuOpen = false;
-    }
+  if (!menuOpen) {
+    menuBtn.classList.add("open");
+    overlay.classList.add("open-list");
+    header.classList.add("close");
+    scrollDown.style.zIndex = "-1";
+    menuOpen = true;
+  } else {
+    menuBtn.classList.remove("open");
+    overlay.classList.remove("open-list");
+    header.classList.remove("close");
+    scrollDown.style.zIndex = "1";
+    menuOpen = false;
+  }
 });
-
-
